92a08c129a2f72d4ad47c80098eed2d57b546f9c
salmalamr <salmalmaribah5@gmail.com>
2026-02-11 10:22:20 +0100

    conflict in tailwind fixing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import AppRoutes from "./routes/routes";
 import "./styles/tailwind.css"
-import { Button } from "./components/ui/button"
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
 <AppRoutes/>
-=======
-omar 
-<Button>Cliquez ici</Button>
-      <Button variant="secondary">Bouton secondaire</Button>
-      <Button variant="destructive">Supprimer</Button>
->>>>>>> origin/main
+
     </div> 
   );
 }
